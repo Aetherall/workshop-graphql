7bfd2708f78c587564a096df0f6c8b9ecbdf5ddf
Aetherall <elieslou.dev@gmail.com>
2022-06-15 14:22:52 +0200

    authentication

--- src/boot.ts
+++ src/boot.ts
@@ -8,11 +8,13 @@ import {
 import { WebSocketServer } from "ws";
 import { useServer } from "graphql-ws/lib/use/ws";
 import { GraphQLSchema } from "graphql";
+import cookieParser from "cookie-parser";
 
 export async function boot(PORT: number, schema: GraphQLSchema) {
   // Create an Express app and HTTP server; we will attach the WebSocket
   // server and the ApolloServer to this HTTP server.
   const app = express();
+  app.use(cookieParser());
   const httpServer = createServer(app);
 
   // Set up WebSocket server.
@@ -25,6 +27,21 @@ export async function boot(PORT: number, schema: GraphQLSchema) {
   // Set up ApolloServer.
   const server = new ApolloServer({
     schema,
+    context: ({ req, res }) => {
+      const setToken = (token: string) =>
+        res.cookie("token", token, {
+          expires: new Date(Date.now() + 900000),
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        });
+
+      return {
+        token: req.cookies.token,
+        setToken,
+        clearToken: () => res.clearCookie("token"),
+      };
+    },
     plugins: [
       // Proper shutdown for the HTTP server.
       ApolloServerPluginDrainHttpServer({ httpServer }),
@@ -42,7 +59,16 @@ export async function boot(PORT: number, schema: GraphQLSchema) {
     ],
   });
   await server.start();
-  server.applyMiddleware({ app });
+  server.applyMiddleware({
+    app,
+    cors: {
+      origin: ["https://studio.apollographql.com"],
+      credentials: true,
+    },
+  });
+
+  // Vulnerability
+  app.set("trust proxy", 1);
 
   // Now that our HTTP server is fully set up, actually listen.
   httpServer.listen(PORT, () => {
--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ abstract class Aggregate {
   abstract id: string;
 }
 
+abstract class Entity {
+  abstract id: string;
+}
+
 abstract class Store<A extends Aggregate> {
   abstract load(id: string): Promise<A | undefined>;
   abstract save(instance: A): Promise<void>;
@@ -32,146 +36,270 @@ class InMemoryStore<A extends Aggregate> extends Store<A> {
   }
 }
 
-class Person extends Aggregate {
-  constructor(public readonly name: string, public age: number) {
-    super();
+/**
+ * In this mock kraaft, the domain will be simplified to 2 BC
+ * - Authentication = User(id, name) -> { Token[] }
+ * - Conversation = Conversation(id, name, members) -> { Message[] }
+ *
+ * IRL its more than likely the Message will not be an entity but its own aggregate
+ */
+
+class ValueObject<T extends string | number> {
+  constructor(public readonly value: T) {}
+
+  serialize() {
+    return this.value;
   }
 
-  get id() {
-    return this.name;
+  static deserialize<T extends new (...args: any[]) => ValueObject<any>>(
+    this: T,
+    value: T extends new (...args: any[]) => ValueObject<infer U> ? U : never
+  ) {
+    return new this(value) as InstanceType<T>;
+  }
+
+  static equals<T extends new (...args: any[]) => ValueObject<any>>(
+    this: T,
+    left: InstanceType<T>,
+    right: InstanceType<T>
+  ) {
+    return left.value === right.value;
   }
 }
 
-class Car extends Aggregate {
-  ownerName?: string;
-  constructor(public readonly greyCardId: string) {
-    super();
-  }
-
-  get id() {
-    return this.greyCardId;
-  }
-
-  registerTo(ownerName: string) {
-    this.ownerName = ownerName;
+class Id extends ValueObject<string> {
+  static generate<T extends new (...args: any[]) => Id>(this: T) {
+    const value = Math.random().toString().substring(2, 8);
+    return new this(value) as InstanceType<T>;
   }
 }
 
-const personStore = new InMemoryStore<Person>();
-const carStore = new InMemoryStore<Car>();
+class UserId extends Id {}
+class Password extends ValueObject<string> {}
 
-const john = new Person("John", 30);
-const jane = new Person("Jane", 25);
-const punto = new Car("12345");
-const multipla = new Car("67890");
+class Token extends ValueObject<string> {
+  static fromPassword(password: Password) {
+    return new this(password.value);
+  }
+}
 
-multipla.registerTo("John");
-punto.registerTo("Jane");
+class Email extends ValueObject<string> {
+  constructor(value: string) {
+    super(value);
 
-personStore.save(john);
-personStore.save(jane);
-carStore.save(punto);
-carStore.save(multipla);
+    if (!Email.isValid(value)) {
+      throw new Error(`DomainError: Invalid email ${value}`);
+    }
+  }
 
-interface PersonType {
-  // belongs in infrastructure
+  static isValid(value: string) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+  }
+}
+
+class FullName extends ValueObject<string> {}
+class ConversationId extends Id {}
+class ConversationTitle extends ValueObject<string> {}
+class MessageId extends Id {}
+class MessageContent extends ValueObject<string> {
+  constructor(value: string) {
+    super(value);
+    if (value.length > 100) {
+      throw new Error(
+        "DomainError: Message content must be less than 100 characters"
+      );
+    }
+  }
+}
+
+class User extends Aggregate {
+  constructor(
+    public readonly userId: UserId,
+    public readonly email: Email,
+    public readonly password: Password, // dont mind the password in clear, not tryna be secure
+    public readonly name: FullName
+  ) {
+    super();
+  }
+
+  static new(email: Email, password: Password, name: FullName) {
+    return new this(UserId.generate(), email, password, name);
+  }
+
+  hasToken(token: Token) {
+    return this.password.value === token.value;
+  }
+
+  authenticate(password: Password) {
+    console.log(password, this.password);
+    if (Password.equals(this.password, password)) {
+      return Token.fromPassword(password);
+    } else {
+      throw new Error("DomainError: Invalid password");
+    }
+  }
+
+  get id() {
+    return this.name.toString();
+  }
+}
+
+class Message extends Entity {
+  constructor(
+    public readonly messageId: MessageId,
+    public readonly author: UserId,
+    public readonly content: MessageContent
+  ) {
+    super();
+  }
+
+  static redact(author: UserId, content: MessageContent) {
+    return new this(MessageId.generate(), author, content);
+  }
+
+  get id() {
+    return this.messageId.toString();
+  }
+}
+
+class Conversation extends Aggregate {
+  constructor(
+    public readonly conversationId: ConversationId,
+    public title: ConversationTitle,
+    public readonly members: UserId[],
+    public readonly messages: Message[]
+  ) {
+    super();
+  }
+
+  publishMessage(author: UserId, content: MessageContent) {
+    const message = Message.redact(author, content);
+    this.messages.push(message);
+  }
+
+  addMember(userId: UserId) {
+    this.members.push(userId);
+  }
+
+  get id() {
+    return this.conversationId.serialize();
+  }
+}
+
+const userStore = new (class extends InMemoryStore<User> {
+  loadByEmail(email: Email) {
+    return this.all().find(
+      (user) => user.email.serialize() === email.serialize()
+    );
+  }
+
+  loadByToken(token: Token) {
+    return this.all().find((user) => user.hasToken(token));
+  }
+})();
+const conversationStore = new InMemoryStore<Conversation>();
+
+interface Context {
+  token?: string;
+  setToken(token: string): void;
+  clearToken(): void;
+}
+
+interface LogInArgs {
+  email: string;
+  password: string;
+}
+
+interface SignUpArgs {
+  email: string;
+  password: string;
   name: string;
-  age: number;
-}
-
-interface CarType {
-  greyCardId: string;
-  ownerName?: string;
 }
 
 const schema = makeExecutableSchema({
   typeDefs: gql`
-    type Person {
-      name(capitalize: Boolean): String!
-      age: Int!
-      cars: [Car!]!
+    type User {
+      id: ID!
+      name: String!
     }
 
-    type Car {
-      greyCardId: String!
-      owner: Person
+    # type Message {
+    #   id: ID
+    #   content: String!
+    #   author: User!
+    # }
+
+    # type Conversation {
+    #   id: ID!
+    #   title: String!
+    #   members: [User!]!
+    # }
+
+    type Mutation {
+      logIn(email: String!, password: String!): User!
+      signUp(email: String!, password: String!, name: String!): User!
+      logOut: Boolean!
     }
 
     type Query {
-      people: [Person!]!
-      cars: [Car!]!
-    }
-
-    type Mutation {
-      buyNewCar(ownerName: String!): Car
-    }
-
-    type Subscription {
-      personCars(ownerName: String!): [Car!]!
+      me: User!
+      # conversations: [Conversation!]!
     }
   `,
   resolvers: {
+    Query: {
+      me: (parent, params, context: Context, info) => {
+        const token = context.token as string;
+        if (!token) {
+          throw new Error("Applicative Error: Not logged in");
+        }
+        const user = userStore.loadByToken(Token.deserialize(token));
+        if (!user) {
+          throw new Error("Applicative Error: Unknown token");
+        }
+        return { id: user.userId.serialize(), name: user.name.serialize() };
+      },
+    },
     Mutation: {
-      buyNewCar: async (
-        parent: never,
-        { ownerName }: { ownerName: string }
-      ) => {
-        const person = await personStore.load(ownerName);
-        if (!person) {
-          throw new Error("Applicative Error: Owner unknown");
+      logIn: async (parent, params: LogInArgs, context: Context, info) => {
+        const { email, password } = params;
+        const user = await userStore.loadByEmail(Email.deserialize(email));
+        if (!user) {
+          throw new Error("Applicative Error: User not found");
         }
 
-        const newCar = new Car(`${Math.random()}`.substring(2, 8));
-        newCar.registerTo(ownerName);
-        carStore.save(newCar);
-        pubsub.publish("CarBought", {
-          greyCardId: newCar.greyCardId,
-          ownerName,
-        });
-        return newCar;
+        const token = user.authenticate(Password.deserialize(password));
+
+        context.setToken(token.serialize());
+        return { id: user.userId.serialize(), name: user.name.serialize() };
       },
-    },
-    Query: {
-      people: () => personStore.all(),
-      cars: () => carStore.all(),
-    },
-    Person: {
-      name: (person: PersonType, { capitalize }) => {
-        if (capitalize) {
-          return person.name.toUpperCase();
+      signUp: async (parent, params: SignUpArgs, context, info) => {
+        const { name, email, password } = params;
+        const { setToken } = context;
+        const existingUser = await userStore.load(name);
+        if (existingUser) {
+          throw new Error("Applicative Error: User already exists");
         }
-        return person.name;
+
+        const user = User.new(
+          Email.deserialize(email),
+          Password.deserialize(password),
+          FullName.deserialize(name)
+        );
+
+        await userStore.save(user);
+
+        const token = user.authenticate(Password.deserialize(password));
+        setToken(token.serialize());
+
+        return {
+          id: user.userId.serialize(),
+          name: user.name.serialize(),
+        };
       },
-      cars: async (person: PersonType) => {
-        // or use a projection / efficient db query to retrieve the right cars
-        const cars = await carStore.all();
-        return cars.filter((car) => car.ownerName === person.name);
-      },
-    },
-    Car: {
-      owner: async (car: CarType) => {
-        // we can bypass the applicative layer when doing CQRS,
-        // but otherwise its better to use a query from the applicative
-        // layer rather than using the store directly
-        // (the logic "if car.ownerName" belongs to the applicative layer)
-        if (car.ownerName) {
-          const owner = await personStore.load(car.ownerName);
-          return owner;
-        }
-      },
-    },
-    Subscription: {
-      personCars: {
-        subscribe: withFilter(
-          () => pubsub.asyncIterator(["CarBought"]),
-          async (payload, variables) => {
-            return payload.ownerName === variables.ownerName;
-          }
-        ),
-        resolve: async ({ ownerName }: { ownerName: string }) => {
-          const cars = await carStore.all();
-          return cars.filter((car) => car.ownerName === ownerName);
-        },
+      logOut: (parent, params, context, info) => {
+        context.clearToken();
+        return true;
       },
     },
   },
